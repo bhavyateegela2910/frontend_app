ff5dd9ccb54b53e6250cae1e586bce8e9f8338f6
BUNTY <bunty@Mac.mshome.net>
2026-03-13 14:10:07 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
+import { useState, createContext } from "react";
 import Header from "./components/Header";
 import Content from "./components/Content";
-import Footer from "./components/Footer"; 
-import Login from "./components/Login"; 
+import Footer from "./components/Footer";
+import Login from "./components/Login";
 import Logout from "./components/Logout";
 import Register from "./components/Register";
-import Orders from "./components/Orders";
 import Cart from "./components/Cart";
-import { BrowserRouter, Route, Routes } from "react-router-dom";  
-
+import Orders from "./components/Orders";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+export const AppContext = createContext();
 function App() {
+  const [user, setUser] = useState({});
+  const [cart,setCart] = useState([])
   return (
-      <div>
-    <BrowserRouter>
-      
-        <Header />
-        <Routes>
-          <Route index element={<Content />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="cart" element={<Cart />} />
-          <Route path="orders" element={<Orders />} />
-          <Route path="logout" element={<Logout />} />
-        </Routes>
-        <Footer />
-     
-     </BrowserRouter>
-     </div>
+    <div>
+      <AppContext.Provider value={{ user, setUser,cart,setCart }}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route index element={<Content />} />
+            <Route path="cart" element={<Cart />} />
+            <Route path="login" element={<Login />} />
+            <Route path="logout" element={<Logout />} />
+            <Route path="register" element={<Register />} />
+            <Route path="orders" element={<Orders />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </AppContext.Provider>
+    </div>
   );
 }
-
 export default App;
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,44 +1,46 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { AppContext } from "../App";
 import axios from "axios";
-import "./content.css";
+import "./Content.css";
+
 const API_URL = import.meta.env.VITE_API_URL;
+
 function Content() {
-  //const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
-  const increment = () => {
-    setCount(count + 1);
-  };
-  const decrement = () => {
-    setCount(count - 1);
-  };
+  const { user, setUser, cart, setCart } = useContext(AppContext);
   const fetchProducts = async () => {
-    const url =`${API_URL}/store`;
+    const url = `${API_URL}/store`;
     const res = await axios.get(url);
     setProducts(res.data);
   };
   useEffect(() => {
     fetchProducts();
   }, []);
-  return (
-    <div className="row">
-      {/* <h3>Products Page</h3> */}
-      {/* {/* /* <button onClick={decrement}>-</button>
-      {count}
-      <button onClick={increment}>+</button>}
-      <hr /> */}
-      
-        {products.map((product) => (
-            <div className="box" >
-                <img src={`${API_URL}/${product.imageUrl}`}width="200px" alt=""/>
-          <h3>{product.name}</h3>
-          <p>{product.desc}</p>
-          <p>{product.price}</p>
-          <p>{product.rating}</p>
-          <p><button>Add TO cart
-            </button></p>
 
-        </div>))}
-      
+  const addToCart = (product) => {
+    const found = cart.find((item) => item._id === product._id);
+    if (!found) {
+      product.quantity = 1;
+      setCart([...cart,product]);
+    }
+  };
+
+  return (
+    <div>
+      <div className="row">
+        {products.map((product) => (
+          <div className="box">
+            <img src={`${API_URL}/${product.imageUrl}`} width="300px" alt="" />
+            <h3>{product.name}</h3>
+            <p>{product.desc}</p>
+            <h4>{product.price}</h4>
+            <p>
+              <button onClick={() => addToCart(product)}>Add to Cart</button>
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
